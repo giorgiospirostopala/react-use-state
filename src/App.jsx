335e8e84031241ce57a6481480c3ac3d9e90abc1
giorgiospirostopala <gio.topala@gmail.com>
2025-02-26 14:49:23 +0100

    add: Card edit + moved useState() in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import Card from './components/Card';
+import { useState } from "react";
+import languages from "./data/languages";
+import Card from "./components/Card";
+import "./index.css";
 
 function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
+
   return (
     <div>
-      <Card />
+      <h1>TITOLO</h1>
+      <div>
+        {languages.map((lang) => (
+          <button
+            key={lang.id}
+            onClick={() => setSelectedLanguage(lang)}
+          >
+            {lang.title}
+          </button>
+        ))}
+      </div>
+      <Card title={selectedLanguage.title} description={selectedLanguage.description} />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,10 @@
-import { useState } from 'react';
-import languages from '../data/languages';
-
-function Card() {
-    const [linguaggioSelezionato, functLinguaggioSelezionato] = useState(languages);
-
+function Card({ title, description }) {
     return (
-        <div>
-            <div>
-                {languages.map(linguaggio => (
-                    <button key={linguaggio.id} onClick={() => functLinguaggioSelezionato(linguaggio)}>
-                        {linguaggio.title}
-                    </button>
-                ))}
-            </div>
-            <div>
-                <h2>{linguaggioSelezionato.title}</h2>
-                <p>{linguaggioSelezionato.description}</p>
-            </div>
-        </div>
+      <div className="card">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
     );
-}
-
-export default Card;
+  }
+  
+  export default Card;
